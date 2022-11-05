f30db6f8285b0607e300529b0dad78b349ad5155
Omar Dominguez <devdor.09@gmail.com>
2022-11-04 19:46:17 -0600

    Agregando logica a menu y cambiando funciones

--- main.py
+++ main.py
@@ -9,19 +9,43 @@ def back_menu():
 
 
 def menu():
-    pass
+    while True:
+        print("""
+        --- Guess the number ---
+        1. Easy
+        2. Medium
+        3. Hard
+        4. Exit
+        """)
+        opc = int(input("Selec an option: "))
+
+        if opc == 1:
+            easy_level()
+        elif opc == 2:
+            medium_level
+        elif opc == 3:
+            hard_level
+        elif opc == 4:
+            break
+        else:
+            print("Select a correct option2")
 
 # Funciones Logicas
-def vidas_limitadas():
+def easy_level():
     pass
 
 
-def vidas_ilimitadas():
+def medium_level():
+    pass
+
+
+def hard_level():
     pass
 
 # Funcion Main
 def main():
-    pass
+    menu()
+    print("Programa Finalizado")
 
 
 if __name__ == '__main__':
