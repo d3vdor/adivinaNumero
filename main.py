af056a4fa9ee068b12409d5f63b0751eba4a61bf
Omar Dominguez <devdor.09@gmail.com>
2022-11-04 19:57:29 -0600

    Agregando logica de juego y primer nivel completado

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import random
 import time
 
 # Funciones Globales
+def logica_juego(a,b):
+    if a < b:
+        print("Digita un numero mas grande")
+    else:
+        print("Digita un numero mas chico")
+
+
 def back_menu():
     pass
 
@@ -11,13 +18,13 @@ def back_menu():
 def menu():
     while True:
         print("""
-        --- Guess the number ---
-        1. Easy
-        2. Medium
-        3. Hard
-        4. Exit
+        --- Menu ---
+        1. Easy Level
+        2. Medium Level
+        3. Hard Level
+        4. Exit 
         """)
-        opc = int(input("Selec an option: "))
+        opc = int(input("Selecciona una opcion: "))
 
         if opc == 1:
             easy_level()
@@ -28,11 +35,21 @@ def menu():
         elif opc == 4:
             break
         else:
-            print("Select a correct option2")
+            print("Digita una opcion correcta")
 
 # Funciones Logicas
 def easy_level():
-    pass
+    print("Adivina el numero del 1 al 100\n")
+    random_number = random.randint(1,100)
+    user_number = int(input("Digite un numero: "))
+    vidas = 0
+    while user_number != random_number:
+        logica_juego(user_number,random_number)
+        user_number = int(input("Digita otro numero: "))
+        vidas += 1
+    print("Adivinaste el numero!!")
+    print(f"Numero de intentos: {vidas}")
+    menu()
 
 
 def medium_level():
@@ -45,7 +62,7 @@ def hard_level():
 # Funcion Main
 def main():
     menu()
-    print("Programa Finalizado")
+    print("\nPrograma Finalizado")
 
 
 if __name__ == '__main__':
