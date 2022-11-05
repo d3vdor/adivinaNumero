35dff4637f849f7ed5898520629889cff3f5a814
Omar Dominguez <devdor.09@gmail.com>
2022-11-04 19:38:18 -0600

    Agregando estructura basica

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# Librerias
+import os
+import random
+import time
+
+# Funciones Globales
+def back_menu():
+    pass
+
+
+def menu():
+    pass
+
+# Funciones Logicas
+def vidas_limitadas():
+    pass
+
+
+def vidas_ilimitadas():
+    pass
+
+# Funcion Main
 def main():
     pass
 
