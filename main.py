3e1e52284b7df615fee2d0ceab6db4e5eab45fcd
Omar Dominguez <devdor.09@gmail.com>
2022-11-07 12:23:07 -0600

    Reduciendo y corrigiendo lineas de codigo

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ def header(mensaje,n_lifes):
     print(f"Vidas Totales: {n_lifes}")
 
 
-def entrada():
+def entrada(vidas):
     random_number = random.randint(1,100)
-    print("vidas totales: 20")
     user_number = int(input("Digite un numero: "))
-    salida(random_number,user_number,10)
+    salida(random_number,user_number,vidas)
+
+
+def logica_juego(a,b):
+    if a < b:
+        print("Digita un numero mas grande")
+    else:
+        print("Digita un numero mas chico")
 
 
 def salida(n_random,n_user,n_lifes):
@@ -27,13 +33,6 @@ def salida(n_random,n_user,n_lifes):
             back_menu()
     back_menu()
     return n_random,n_user
-
-
-def logica_juego(a,b):
-    if a < b:
-        print("Digita un numero mas grande")
-    else:
-        print("Digita un numero mas chico")
 
 
 def back_menu():
@@ -73,17 +72,17 @@ def menu():
 # Funciones Logicas
 def easy_level():
     header("Easy Level",20)
-    entrada()
+    entrada(20) # Cantidad de vidas del nivel
 
 
 def medium_level():
     header("Medium Level",10)
-    entrada()
+    entrada(10)
 
 
 def hard_level():
     header("Hard Level",5)
-    entrada()
+    entrada(5)
 
 # Funcion Main
 def main():
